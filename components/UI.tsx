e2916f49c7c2313f7a1466323dd3fa3b5fba8297
kiryuin0121 <162397520+kiryuin0121@users.noreply.github.com>
2026-04-16 09:27:08 +0900

    パーツの色変更UIを実装した
    デザインは後で整える。

--- components/UI.tsx
+++ components/UI.tsx
@@ -24,6 +24,20 @@ const SHOE_PART_LABEL: Record<ShoePart, string> = {
   band: "シュータン",
   patch: "バックタブ",
 };
+const COLORS = [
+  { label: "ホワイト", code: "#ffffff" },
+  { label: "ブラック", code: "#000000" },
+  { label: "グレー", code: "#d1d5db" },
+  { label: "レッド", code: "#ef4444" },
+  { label: "オレンジ", code: "#f97316" },
+  { label: "イエロー", code: "#eab308" },
+  { label: "グリーン", code: "#22c55e" },
+  { label: "シアン", code: "#06b6d4" },
+  { label: "ブルー", code: "#3b82f6" },
+  { label: "インディゴ", code: "#6366f1" },
+  { label: "パープル", code: "#a855f7" },
+  { label: "ピンク", code: "#ec4899" },
+];
 const UI = () => {
   const shoeConfig = useAtomValue(shoeConfigAtom);
   const setShoePartColor = useSetAtom(setShoePartColorAtom);
@@ -33,9 +47,9 @@ const UI = () => {
       className={`absolute inset-0 z-10 w-screen h-screen pointer-events-none`}
     >
       <section
-        className={`absolute bottom-0 left-0 pointer-events-auto w-screen min-h-1/4 bg-neutral-100 `}
+        className={`absolute bottom-0 left-0 pointer-events-auto w-screen min-h-1/4 bg-neutral-100 space-y-8`}
       >
-        <div className={`flex justify-center items-center gap-x-4`}>
+        <div className={`flex justify-center items-center gap-x-4 text-lg`}>
           <button
             className={`cursor-pointer`}
             onClick={() =>
@@ -46,7 +60,11 @@ const UI = () => {
           >
             ←
           </button>
+          <div className={`flex gap-x-2`}>
+
           <h2>{SHOE_PART_LABEL[SHOE_PARTS[slideNum]]}</h2>
+          <span className={`text-neutral-500`}>{slideNum+1}/{SHOE_PARTS.length}</span>
+          </div>
           <button
             className={`cursor-pointer`}
             onClick={() =>
@@ -57,7 +75,25 @@ const UI = () => {
           </button>
         </div>
 
-        <ul></ul>
+        <ul className={`flex gap-8 justify-center items-center`}>
+          {
+            COLORS.map((color)=>(
+              <li key={color.code}>
+                <button
+                onClick={() =>
+                  setShoePartColor({
+                    part: SHOE_PARTS[slideNum],
+                    color:color.code,
+                  })
+                }
+                style={{backgroundColor:color.code}}
+                className={`h-8 aspect-square rounded-full cursor-pointer`}
+                />
+                <p className={`text-xs`}>{color.label}</p>
+              </li>
+            ))
+          }
+        </ul>
       </section>
     </div>
   );
